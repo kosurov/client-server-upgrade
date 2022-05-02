00535948896aa0553651628eb2108ab2189c6a45
kosurov <kosurov@inbox.ru>
2022-05-02 12:18:10 +0300

    Added try-with-resources

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,27 +6,31 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        int port = 8105;
-        ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-
-        while (true) {
-            Socket clientSocket = serverSocket.accept(); // ждем подключения
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println("New connection accepted");
-
-            out.println("Write your name");
-            String name = in.readLine();
-            out.println("Are you child? (yes/no)");
-            String isChild = in.readLine();
-            // Чтобы сообшить клиенту, что это последнее сообщение от сервера, это сообщение должно содержать "Connection closed"
-            if (isChild.equals("yes")) {
-                out.println(String.format("Welcome to the kids area, %s! Let's play!Connection closed", name));
-            } else {
-                out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or working day!Connection closed", name));
+    public static void main(String[] args) {
+        int port = 8105; // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    System.out.println("New connection accepted");
+                    out.println("Write your name");
+                    String name = in.readLine();
+                    out.println("Are you child? (yes/no)");
+                    String isChild = in.readLine();
+                    // Чтобы сообщить клиенту, что это последнее сообщение от сервера, это сообщение должно содержать "Connection closed"
+                    if (isChild.equals("yes")) {
+                        out.println(String.format("Welcome to the kids area, %s! Let's play!Connection closed", name));
+                    } else {
+                        out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or working day!Connection closed", name));
+                    }
+                    System.out.println("Connection closed");
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
             }
-            System.out.println("Connection closed");
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 }
